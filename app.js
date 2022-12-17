a7209e3dff1fd8b165ecb894c1cfbcee6851d4b1
CamilaRB <crodriguesbalieiro94@gmail.com>
2022-12-16 20:36:52 -0400

    Css + JavaScript

---
+++ app.js
@@ -0,0 +1,55 @@
+console.log("Meu nome eh Camila")
+document.getElementById("titulos").innerHTML = "Camila Rodrigues"
+
+/*let nome = "Camila"
+let idade = "28"
+let sexo = true
+
+console.log (nome)
+console.log (idade)
+console.log (sexo)
+
+//numer = numero
+//strin = texto
+//boolean = false/true
+/*
+soma = +
+subt = -
+divi =/
+mult = *
+rest = %
+meno = <
+maio = >
+igual = ===
+
+*/
+/*
+let resultado = 10+2;
+console.log ("Soma:",resultado)
+
+resultado = 10 - 2;
+console.log ("Subtração:", resultado)
+
+resultado = 10 / 2
+console.log ("Divisão:",resultado)
+
+resultado = 10 * 2
+console.log ("Multiplicação:",resultado)
+
+resultado = 10 % 2
+console.log ("Resto:",resultado)
+
+resultado = 4 < 5
+console.log ("4 eh Menor que 5", resultado);
+
+resultado = 4 > 5
+console.log ("4 eh Maior que 5", resultado);
+
+const valor1 = 10;
+const valor2 = 10;
+
+if (valor1 - valor2 ===10){
+    console.log("Valor eh igual a 10");
+}else{
+    console.log("Valor não é igaul a 10")
+}*/
